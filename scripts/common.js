ff0a407ee5146761b683d60584a5754129472957
武藤桂子 <k-muto@astaster.jp>
2016-09-08 14:24:23 +0900

    インラインフレームに変更

--- scripts/common.js
+++ scripts/common.js
@@ -17,37 +17,84 @@
 
     btn01.on('click', function() {
       $(this).addClass('is-childOpen'); //第1階層ボタンにクラス付与、第2階層メニューが表示される
-      backBtn.prop('disabled', false); //backボタンを有効化
-      homeBtn.prop('disabled', false); //homeボタンを有効化
-      //return false;
+      if ($(this).attr('href') == '#') {
+        backBtn.prop('disabled', false); //backボタンを有効化
+        homeBtn.prop('disabled', false); //homeボタンを有効化
+      }
 
       var btn02 = $(this).next().find('.btn02') //第2階層ボタン
 
       btn02.on('click', function() {
         if ($(this).attr('href') == '#') {
           $(this).addClass('is-childOpen'); //第2階層ボタンにクラス付与、第3階層メニューが表示される
-          //return false;
+
 
           var btn03 = $(this).next().find('.btn03'); //第3階層ボタン
 
           btn03.on('click', function() {
             if ($(this).attr('href') == '#') {
               $(this).addClass('is-childOpen'); //第3階層ボタンにクラス付与、第4階層メニューが表示される
-              //return false;
+
             }
           });
         }
       });
 
     });
+
+    //サイドバーの高さ調整（スマホ）
+/*    if (window.matchMedia('(max-width: 768px)').matches) {
+
+
+      var biggestHeight = '0';
+      var winH = $(window).height();
+      $('#js-navSide ul').each(function() {
+        if ($(this).outerHeight() > winH) {
+          //biggestHeight = $(this).outerHeight()
+          $(this).css({
+            'height';100+'%'
+          })
+        }else{
+          $(this).css({
+            'height';100+'%'
+          })
+        }
+      });
+      $('#js-navSide').outerHeight(biggestHeight);
+
+
+    };*/
     /*サイドバー表示ボタン（スマホ）
     ==============================================*/
     $('#js-btnNav').on('click', function() {
       $(this).toggleClass('is-side');
       $('#js-side').toggleClass('is-side');
-      //$('.sec.is-active').toggleClass('is-active');
     });
 
+    /*    var current_scrollY = $(window).scrollTop();
+        $('#js-btnNav').toggle(
+
+          function() {
+            $(this).addClass('is-side');
+            $('#js-side').addClass('is-side');
+
+            //ページの位置を固定（サイドバースクロール時にスクロールさせないため）
+            $('#js-content').css({
+              position: 'fixed',
+              top: -1 * current_scrollY
+            })
+
+          },
+          function() {
+            $(this).removeClass('is-side');
+            $('#js-side').removeClass('is-side');
+
+            $('#js-content').attr({ style: '' });
+            $('html, body').prop({ scrollTop: current_scrollY });
+          }
+
+        );
+    */
     /*ページ
     ==============================================*/
     $('.btn').on('click', function() {
@@ -65,18 +112,19 @@
     });
 
     //コンテンツ領域の高さを一番高さのあるページ要素に合わせる
-    if (window.matchMedia('(min-width: 768px)').matches) { //PC
-      /*var biggestHeight = '0';
+    /*if (window.matchMedia('(min-width: 768px)').matches) { //PC
+      var biggestHeight = '0';
       $('.sec').each(function() {
         if ($(this).outerHeight() > biggestHeight) {
           biggestHeight = $(this).outerHeight()
         }
       });
-      $('#js-content').outerHeight(biggestHeight);*/
+      $('#js-content').outerHeight(biggestHeight);
     } else { //SP
-      /*var biggestHeight = $('.sec').outerHeight();
-      $('#js-content').outerHeight(biggestHeight);*/
-    };
+      var biggestHeight = $('.sec').outerHeight();
+      $('#js-content').outerHeight(biggestHeight);
+    };*/
+
     /*フッター
     ==============================================*/
     backBtn.on('click', function() {
@@ -87,23 +135,23 @@
       } else { //第3階層が非表示の時、第1階層ボタンのクラスを削除、第2階層メニューが非表示なる
         $('.btn01.is-childOpen').removeClass('is-childOpen');
         $('.btn').removeClass('is-current');
-        $('.sec').removeClass('is-active');
-        $('#page00').addClass('is-active');
-        backBtn.attr('disabled', 'disabled'); //backボタンを無効化
-        homeBtn.attr('disabled', 'disabled'); //homeボタンを無効化
+        disable();
       }
     });
 
     homeBtn.on('click', function() {
       $('.btn').removeClass('is-childOpen is-current'); //サイドバーのボタンのonをすべて削除
+      $('#js-btnNav,#js-side').removeClass('is-side');
+      disable();
+    });
+
+    //ユーザー関数
+    function disable() {
       $('.sec').removeClass('is-active');
       $('#page00').addClass('is-active');
       backBtn.attr('disabled', 'disabled'); //backボタンを無効化
       homeBtn.attr('disabled', 'disabled'); //homeボタンを無効化
-
-       $('#js-btnNav,#js-side').removeClass('is-side');
-    });
-
+    }
 
   });
 }(jQuery));
